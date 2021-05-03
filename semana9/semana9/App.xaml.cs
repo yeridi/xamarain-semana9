bf162c97e58c2428e734e742dfd1f530d1d9ec77
yeridi <yeridicrhis2002@gmail.com>
2021-05-03 17:15:51 -0500

    final commit

---
+++ semana9/semana9/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using semana9.Views;
+using Xamarin.Forms;
+
+namespace semana9
+{
+    public partial class App : Application
+    {
+        public static float ScreenHeight { get; set; }
+        public static float ScreenWidth { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SwipeDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ semana9/semana9/TapDemo.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana9.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TapDemo : ContentPage
+    {
+        int tapCount;
+        readonly Label label;
+        public TapDemo()
+        {
+            InitializeComponent();
+            var image = new Image
+            {
+                Source = "dog.jpg",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            var tapGestureRecognier = new TapGestureRecognizer();
+            tapGestureRecognier.NumberOfTapsRequired = 2;
+            tapGestureRecognier.Tapped += OnTapGestureRecognizer;
+            image.GestureRecognizers.Add(tapGestureRecognier);
+
+            label = new Label
+            {
+                Text = "Tap the photo",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Content = new StackLayout 
+            { 
+                Padding = new Thickness(20, 100),
+                Children =
+                { 
+                    image,
+                    label
+                }
+            };
+        }
+        void OnTapGestureRecognizer(object sender, EventArgs args) 
+        {
+            tapCount++;
+            label.Text = String.Format("{0} tap{1} so far!",
+                    tapCount,
+                    tapCount == 1 ? "" : "s");
+
+            var imageSender = (Image)sender;
+            //look the changue of the image
+            if (tapCount % 2 == 0)
+            {
+                imageSender.Source = "dog.jpg";
+            }
+            else 
+            {
+                imageSender.Source = "wolf.jpg";
+            };
+        }
+    }
+}
